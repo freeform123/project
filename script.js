d0bcf8aa4116c26ff97e8e27b748bc6710ce2292
Dimon <vexusfree@gmail.com>
2021-09-07 20:59:26 +0300

    ssdd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const numberOfFilims = +prompt('Сколько фильмов вы посмотрели?', '');
+'use strict';
+const numberOfFilims = prompt('Сколько фильмов вы посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilims,
@@ -7,12 +8,28 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;    
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >=30) {
+    console.log ("Вы киноман");
+} else {
+    console.log("Произошла ошибка!");
+
+}
 console.log(personalMovieDB);
